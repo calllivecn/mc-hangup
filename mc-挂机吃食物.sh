753c85cb66a4ae28bf793853a13a88b2e7cfe56a
calllivecn <calllivecn@outlook.com>
2017-12-26 10:30:46 +0800

    修改：mc-挂机吃食物.sh 修改：mc_mousedown.sh

--- mc-挂机吃食物.sh
+++ mc-挂机吃食物.sh
@@ -8,20 +8,35 @@ safe_exit(){
 	xdotool mouseup 3
 	exit 0
 }
-trap safe_exit SIGINT
+trap safe_exit SIGINT SIGTERM
 
-WID=$(xdotool search --name 'Minecraft 1.12.2')
+	WID=$(xdotool search --name 'Minecraft 1.12.2')
 
-xdotool windowfocus $WID
+	xdotool windowfocus $WID
 
-xdotool key Escape 
-sleep 1
+	xdotool key Escape 
+	sleep 1
 #xdotool mousedown 1
+
+send(){
+xdotool key t
+sleep 0.5
+xdotool type --delay 150 "自动挂机中。。。"
+sleep 0.5
+xdotool key Return
+sleep $[60 * 3]
+}
 
 while :
 do
-	sleep $[60*30]
 	xdotool mousedown 3
 	sleep 5
 	xdotool mouseup 3
+
+	for i in {1..10}
+	do
+		send
+	done
+
+	#sleep $[60*30]
 done
