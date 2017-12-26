373857b3259cb014d167fb614be2661d644ad950
calllivecn <c-all@qq.com>
2017-12-26 18:46:37 +0800

    修改：mc-挂机吃食物.sh 调整延迟

--- mc-挂机吃食物.sh
+++ mc-挂机吃食物.sh
@@ -10,33 +10,75 @@ safe_exit(){
 }
 trap safe_exit SIGINT SIGTERM
 
+getFocus(){
+	SOURCE_WID=$(xdotool getwindowfocus)
+}
+
+reconveryFocus(){
+	sleep 0.1
+	xdotool key t
+	sleep 0.1
+	xdotool windowfocus "$SOURCE_WID"
+}
+
+getMcFocus(){
 	WID=$(xdotool search --name 'Minecraft 1.12.2')
+}
 
-	xdotool windowfocus $WID
-
-	xdotool key Escape 
-	sleep 1
-#xdotool mousedown 1
+notify(){
+	notify-send "接下来的10秒钟内，不要动键盘或鼠标。"
+	sleep 5
+}
 
 send(){
-xdotool key t
-sleep 0.5
-xdotool type --delay 150 "自动挂机中。。。"
-sleep 0.5
-xdotool key Return
-sleep $[60 * 3]
+	notify
+	getFocus
+
+	getMcFocus
+
+	xdotool windowfocus $WID
+	sleep 0.1
+	xdotool key Escape 
+	sleep 0.1
+
+	xdotool key t
+	sleep 0.1
+	xdotool type --delay 150 "自动挂机中。。。"
+	sleep 0.1
+	xdotool key Return
+
+	reconveryFocus
+
+	sleep $[60 * 3]
+	#sleep 45
+}
+
+eat(){
+	notify
+	getFocus
+	getMcFocus
+
+	xdotool windowfocus $WID
+	sleep 0.1
+	xdotool key Escape 
+	sleep 0.1
+
+	xdotool mousedown 3
+	sleep 3
+	xdotool mouseup 3
+
+	reconveryFocus
 }
 
 while :
 do
-	xdotool mousedown 3
-	sleep 5
-	xdotool mouseup 3
 
 	for i in {1..10}
 	do
 		send
 	done
+
+	eat
 
 	#sleep $[60*30]
 done
