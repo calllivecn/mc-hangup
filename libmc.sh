9934def5bfb7a223ccc941e5586cfed7341f463a
calllivecn <c-all@qq.com>
2018-01-01 15:32:09 +0800

    新文件：libmc.sh 修改：mc-chat-helper.sh 修改：mc-input.sh 修改：mc-mouse1down.sh 修改：mc-挂机吃食物.sh 修改：mc-挂机钓鱼.sh

---
+++ libmc.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# date 2018-01-01 14:58:47
+# author calllivecn <c-all@qq.com>
+
+
+set -e
+
+delay='xdotool sleep 0.1'
+
+MC_NAME='Minecraft 1.12.2'
+
+which xdotool &>/dev/null || (echo '错误：xdotool 程序未安装。' >&2; zenity --error --text '错误：xdotool 程序未安装' --title 'Minecraft 中文聊天辅助工具'; exit 2)
+
+WIN=$(xdotool search --name "$MC_NAME")
+
+if test -z "$WIN";then
+	echo not found "$WIN_NAME"
+	exit 1
+else
+	xdotool windowfocus $WIN
+fi
+
+ctrl_space(){
+	xdotool sleep 0.5
+	xdotool key Ctrl+space
+}
+
+
+getFocus(){
+	SOURCE_WIN=$(xdotool getwindowfocus)
+	eval $(xdotool getmouselocation --shell)
+}
+
+reconveryFocus(){
+	$delay
+	xdotool key t
+	$delay
+	xdotool windowfocus "$SOURCE_WIN"
+	$delay
+	xdotool mousemove --screen $SCREEN $X $Y
+}
+
+getMcFocus(){
+	WIN=$(xdotool search --name "$MC_NAME")
+	if test -z $WIN;then
+		echo not found $MC_NAME
+		exit 1
+	fi
+}
+
+notify(){
+	notify-send "接下来的10秒钟内，不要动键盘或鼠标。"
+	xdotool sleep 5
+}
+
+send(){
+	notify
+	getFocus
+
+	getMcFocus
+
+	xdotool windowfocus $WIN
+	$delay
+	xdotool key Escape 
+	$delay
+
+	xdotool key t
+	$delay
+	xdotool type --delay 150 "自动挂机中。。。"
+	$delay
+	xdotool key Return
+
+	reconveryFocus
+
+	xdotool sleep $[60 * "$MINTUE"]
+}
+
+eat(){
+	notify
+	getFocus
+	getMcFocus
+
+	xdotool windowfocus $WIN
+	$delay
+	xdotool key Escape 
+	$delay
+	xdotool key ${food}
+	$delay
+	xdotool mousedown 3
+	xdotool sleep 3
+	xdotool mouseup 3
+
+	reconveryFocus
+}
