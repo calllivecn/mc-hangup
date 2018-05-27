882e9fc53126bf2c067e852aa15b4ddaaf3ef158
calllivecn <calllivecn@gmail.com>
2018-05-27 17:42:14 +0800

    修改：libmc.sh 修改：mc-1.12.desktop 修改：mc-1.12.sh 修改：mc-chat-helper.sh 修改：mc-eatfood.sh 修改：mc-fishing.sh 修改：mc-input.sh 修改：mc-mouse1down.sh

--- mc-fishing.sh
+++ mc-fishing.sh
@@ -17,37 +17,23 @@ program=$(dirname ${0})
 fishingRod=${1:-1}
 food=${2:-2}
 
-if echo "$fishingRod" | grep -qE '[1-9]' && echo "$food" |grep -qE '[1-9]';then
-	:
-else
+usage(){
 	echo "钓鱼杆的食物必须是物品栏上的一个位置：1~9 之间。"
 	echo "例子：${0##*/} 6 4"
 	exit 1
+}
+
+if echo "$fishingRod" | grep -qE '[1-9]' && echo "$food" |grep -qE '[1-9]';then
+	:
+else
+	usage
 fi
 
-echo "默认使用物品栏第:${fishingRod}格为钓鱼杆"
-echo "默认使用物品栏第:${food}格为食物"
+echo "默认使用物品栏第:1格为钓鱼杆"
+echo "默认使用物品栏第:2格为食物"
 echo
 echo "当前使用物品栏第:${fishingRod}格为钓鱼杆"
 echo "当前使用物品栏第:${food}格为食物"
-
-
-delay='xdotool sleep 0.1'
-
-eat(){
-
-	$delay
-	xdotool key "$food"
-
-	$delay
-	xdotool mousedown 3
-	xdotool sleep 3
-	xdotool mouseup 3
-
-	$delay
-	xdotool key "$fishingRod"
-}
-
 
 down_up3(){
 	$delay
@@ -58,6 +44,10 @@ down_up3(){
 	xdotool mouseup 3
 }
 
+if [ "$1"x = "-h" ] || [ "$1"x = "--help"x ];then
+	usage
+fi
+
 
 xdotool windowfocus $WIN
 $delay
@@ -67,7 +57,7 @@ xdotool key "$fishingRod"
 
 while :
 do
-	eat
 	down_up3
+	eat
 done
 
