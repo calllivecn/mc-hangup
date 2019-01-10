b1c540a3459d650dd6206814261aaad6271b587a
calllivecn <calllivecn@outlook.com>
2019-01-10 15:38:38 +0800

    新文件：.gitignore 修改：  mc-checkupdate.py 新文件：version_manifest.json

--- mc-checkupdate.py
+++ mc-checkupdate.py
@@ -33,6 +33,28 @@ def check(data):
     #print("资源json地址：", version.get(lstest_))
     return latest_release, latest_snapshot
 
+def get_time(data, release_id):
+
+    versions = json.loads(data).get("versions")
+
+    for re_id in versions:
+        if re_id.get("id") == release_id:
+            releasetime = re_id.get("releaseTime")
+            time = re_id.get("time")
+            break
+
+
+    return releasetime, time
+
+
+def test():
+    with open('version_manifest.json') as f:
+        data = f.read()
+
+    releasetime, time = get_time(data,"1.13.2")
+    print("版本发布时间:",releasetime,"更新时间:",time)
+    
+#test();exit(0)
 
 
 if __name__ == "__main__":
@@ -41,8 +63,8 @@ if __name__ == "__main__":
                                     description="检查我的世界最新版本。")
     
     parse.add_argument("-n", action="store_true", help="只输出版本字符串。")
-
     parse.add_argument("--shell", action="store_true", help="输出可用bash格式。")
+    parse.add_argument("--time", action="store_true", help="输出版本时间。")
 
     groups = parse.add_mutually_exclusive_group()
 
@@ -65,6 +87,8 @@ if __name__ == "__main__":
 
     latest_release, latest_snapshot = check(json_data)
 
+    releasetime_release, time_release = get_time(json_data,latest_release)
+    releasetime_snapshot, time_snapshot = get_time(json_data,latest_snapshot)
 
     if args.shell:
         print(f"RELEASE={latest_release}")
@@ -72,10 +96,10 @@ if __name__ == "__main__":
     else:
 
         if args.release:
-            put_release(latest_release)
+            put_release(latest_release + " 版本发布时间:" + releasetime_release + " 更新时间:" + time_release)
         elif args.snapshot:
-            put_snapshot(latest_snaphost)
+            put_snapshot(latest_snapshot + " 版本发布时间:" + releasetime_snapshot + " 更新时间:" + time_snapshot)
         else:
-            put_release(latest_release)
-            put_snapshot(latest_snapshot)
+            put_release(latest_release + " 版本发布时间:" + releasetime_release + " 更新时间:" + time_release)
+            put_snapshot(latest_snapshot + " 版本发布时间:" + releasetime_snapshot + " 更新时间:" + time_snapshot)
 
