1ec95b04b317ef112094e71cd044b780a654cc72
calllivecn <c-all@qq.com>
2019-01-10 14:37:18 +0800

    修改：mc-checkupdate.py --------------------------------- usage:  Usage: mc-checkupdate.py [--release|--snapshot|--all] [--shell]
    检查我的世界最新版本。
    
    optional arguments:
      -h, --help  show this help message and exit
      -n          只输出版本字符串。
      --shell     输出可用bash格式。
      --all       检查正式版和快照版。
      --release   只检查正式版。
      --snapshot  只检查快照版。

--- mc-checkupdate.py
+++ mc-checkupdate.py
@@ -6,6 +6,7 @@
 
 import sys
 import json
+import argparse
 from urllib import request
 
 
@@ -16,8 +17,6 @@ def download(url=URL):
     data = html.read()
     return data.decode()
 
-
-
 def check(data):
     json_data = json.loads(data)
 
@@ -27,14 +26,56 @@ def check(data):
     latest_snapshot = latest.get("snapshot")
     versions = json_data.get("versions")
     
-    print("最新正式版：", latest_release)
+    #print("最新正式版：", latest_release)
     #print("资源json地址：", version.get(lstest_release))
 
-    print("最新快照版：", latest_snapshot)
+    #print("最新快照版：", latest_snapshot)
     #print("资源json地址：", version.get(lstest_))
+    return latest_release, latest_snapshot
 
 
 
-json_data = download()
+if __name__ == "__main__":
+    
+    parse = argparse.ArgumentParser(usage=" Usage: %(prog)s [--release|--snapshot|--all] [--shell]",
+                                    description="检查我的世界最新版本。")
+    
+    parse.add_argument("-n", action="store_true", help="只输出版本字符串。")
 
-check(json_data)
+    parse.add_argument("--shell", action="store_true", help="输出可用bash格式。")
+
+    groups = parse.add_mutually_exclusive_group()
+
+    groups.add_argument("--all",action="store_true", help="检查正式版和快照版。")
+    groups.add_argument("--release",action="store_true",help="只检查正式版。")
+    groups.add_argument("--snapshot",action="store_true",help="只检查快照版。")
+
+
+    args = parse.parse_args()
+    #print(args)
+
+    if args.n:
+        put_release = print
+        put_snapshot = print
+    else:
+        put_release = lambda t: print("最新正式版:",t)
+        put_snapshot = lambda s: print("最新快照版:",s)
+
+    json_data = download()
+
+    latest_release, latest_snapshot = check(json_data)
+
+
+    if args.shell:
+        print(f"RELEASE={latest_release}")
+        print(f"SNAPSHOT={latest_snapshot}")
+    else:
+
+        if args.release:
+            put_release(latest_release)
+        elif args.snapshot:
+            put_snapshot(latest_snaphost)
+        else:
+            put_release(latest_release)
+            put_snapshot(latest_snapshot)
+
