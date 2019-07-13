49a246674eef91ce298b174edcb1843841e579f3
calllivecn <c-all@qq.com>
2019-07-13 21:42:43 +0800

    修改：mc-mouse1down.sh 搞定

--- mc-mouse1down.sh
+++ mc-mouse1down.sh
@@ -10,7 +10,7 @@ safe_exit(){
 	exit 0
 
 }
-trap safe_exit SIGINT SIGTERM ERR EXIT
+trap safe_exit SIGINT SIGTERM ERR
 
 # quote libmc.sh
 . "$(dirname ${0})"/libmc.sh
@@ -22,4 +22,7 @@ xdotool key Escape
 $delay
 xdotool mousedown 1
 
-wait
+while :
+do
+	xdotool sleep 3
+done
