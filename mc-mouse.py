76f2738c6f1d56d4c13af65a19974eda098858ae
calllivecn <calllivecn@outlook.com>
2019-09-12 23:51:40 +0800

    修改：mc-mouse.py +def hangup(kbm, args): +    print("功能还没完成，使用其他功能吧！")

--- mc-mouse.py
+++ mc-mouse.py
@@ -17,25 +17,26 @@ def eatfood(kbm, food):
     """
 
     kbm.key(food)
-    kbm.mousedown("left")
+    kbm.mousedown("right")
     time.sleep(3)
-    kbm.mouseup("left")
+    kbm.mouseup("right")
 
 def fishing(kbm, args):
 
     # 切换物品栏，选项钓鱼竿
     fishingrod = args.number[0]
-    food = args.unmber[1]
+    food = args.number[1]
 
     kbm.key(fishingrod)
 
     try:
-        input("按CTRL+C结束。")
-        for _ in range(40*60): # 40 分钟吃一次食物。
-            kbm.mouseclick("right")
-            time.sleep(1)
+        print("按CTRL+C结束。")
+        while True:
+            for _ in range(40*60): # 40 分钟吃一次食物。
+                kbm.mouseclick("right")
+                time.sleep(1)
 
-        eatfood(kbm, food)
+            eatfood(kbm, food)
 
     except KeyboardInterrupt:
         print()
@@ -43,12 +44,15 @@ def fishing(kbm, args):
 def mouseLeftDown(kbm, args):
 
     try:
+        print("按CTRL+C结束。")
         kbm.mousebtndown("left")
-        input("按CTRL+C结束。")
     except KeyboardInterrupt:
         kbm.mousebtnup("left")
         print()
 
+
+def hangup(kbm, args):
+    print("功能还没完成，使用其他功能吧！")
 
 
 def main():
@@ -66,15 +70,24 @@ def main():
     parse_fishing.add_argument("number", nargs=2, choices=[str(x) for x in range(10)],
                                 help="物品栏编号，0~9。")
 
-    parse_mouseleftdown.set_defaults(func=mouseLeftDown)
+    parse_hangup = subparse.add_parser("hangup", help="长时间挂机，进食，产生活动：移动，发信息。")
+    parse_hangup.add_argument("-f", "--food", type=int, required=True, help="食物编号")
+    parse_hangup.add_argument("-m", "--move", action="store_true", help="是否随机按下1秒W,S,A,D模拟移动。")
+    parse_hangup.add_argument("-t", "--time", type=int, default=30, help="随机移动间隔时间")
+    #parse_hangup.add_argument("--message", help="是否随机发送一个挂机消息。(只能是)")
+    
 
-    parse_fishing.set_defaults(func=fishing)
+    parse_mouseleftdown.set_defaults(func=lambda arg: mouseLeftDown(kbm, arg))
+
+    parse_fishing.set_defaults(func=lambda arg: fishing(kbm, arg))
+
+    parse_hangup.set_defaults(func=lambda arg: hangup(kbm, arg))
 
     args = parse.parse_args()
-    print(args);exit(0)
+    #print(args);exit(0)
 
     kbm = libkbm.VirtualKeyboardMouse()
-    args.func(kbm, args)
+    args.func(args)
 
 
 if __name__ == "__main__":
