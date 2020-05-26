f3cfcb7244f289dc4b31740293244fa0120271d8
calllivecn <calllivecn@outlook.com>
2020-05-26 10:09:38 +0800

    添加 --no-compile

--- build2freeze-mc-mouse.sh
+++ build2freeze-mc-mouse.sh
@@ -20,7 +20,7 @@ trap "safe_exit" SIGTERM SIGINT EXIT
 
 cp mc-mouse.py "$TMP/mcmouse.py"
 
-pip3 install --target "$TMP" git+https://github.com/calllivecn/keyboardmouse@master
+pip3 install --no-compile --target "$TMP" git+https://github.com/calllivecn/keyboardmouse@master
 
 python3 -m zipapp "$TMP" -c -o mc-mouse.pyz -m "mcmouse:main"
 
