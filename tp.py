a917dc8804c5b8d67547fa90005cd321d7ce8dde
calllivecn <calllivecn@outlook.com>
2021-02-06 17:31:27 +0800

    添加上了颜色，.tp <收藏点> 还没开好，以及想添加上扣经验。

--- tp.py
+++ tp.py
@@ -8,7 +8,7 @@ import os
 import json
 from pathlib import Path
 
-from mcdreforged.api.rtext import RText, RColor, RAction
+from mcdreforged.api.rtext import RText, RColor, RAction, RStyle, RTextList
 from mcdreforged.command.builder.command_node import Literal, QuotableText, Text, GreedyText, Integer
 
 PLUGIN_METADATA = {
@@ -33,17 +33,6 @@ plugin_id = PLUGIN_METADATA["id"]
 cmdprefix = "." + plugin_id
 config_dir = Path(os.path.dirname(os.path.dirname(__file__)), "config", plugin_id)
 
-
-def process_coordinate(text):
-	data = text[1:-1].replace('d', '').split(', ')
-	data = [(x + 'E0').split('E') for x in data]
-	return tuple([float(e[0]) * 10 ** int(e[1]) for e in data])
-
-
-def process_dimension(text):
-	return text.replace(re.match(r'[\w ]+: ', text).group(), '', 1)
-
-
 def display(server, name, position, dimension):
 	x, y, z = position
 	dimension_convert = {
@@ -51,49 +40,6 @@ def display(server, name, position, dimension):
 		'minecraft:the_nether': '-1',
 		'minecraft:the_end': '1',
 	}
-	dimension_color = {
-		'0': '§2',
-		'-1': '§4',
-		'1': '§5'
-	}
-	dimension_display = {
-		'0': {'translate': 'createWorld.customize.preset.overworld'},
-		'-1': {'translate': 'advancements.nether.root.title'},
-		'1': {'translate': 'advancements.end.root.title'}
-	}
-	if dimension in dimension_convert:  # convert from 1.16 format to pre 1.16 format
-		dimension = dimension_convert[dimension]
-	texts = [
-		'',
-		{
-			'text': '§e{}'.format(name),
-			'clickEvent':
-			{
-				'action': 'run_command',
-				'value': '/execute at {0} run tp {0}'.format(name)
-			}
-		},
-		'§r @ ',
-		dimension_color[dimension],  # hacky fix for voxelmap yeeting text color in translated text 
-		dimension_display[dimension],
-		' §b[x:{}, y:{}, z:{}]§r'.format(int(x), int(y), int(z))
-	]
-	if dimension in ['0', '-1']:  # coordinate convertion between overworld and nether
-		dimension_opposite = '-1' if dimension == '0' else '0'
-		x, z = (x / 8, z / 8) if dimension == '0' else (x * 8, z * 8)
-		texts.extend([
-			' §7->§r ',
-			dimension_color[dimension_opposite],
-			dimension_display[dimension_opposite],
-			' ({}, {}, {})'.format(int(x), int(y), int(z))
-		])
-
-	t = json.dumps(texts)
-	serverlogger.info(f"texts ------------------>\n{t}")
-	server.execute('tellraw @a {}'.format(t))
-
-	#if HIGHLIGHT_TIME > 0:
-		#server.execute('effect give {} minecraft:glowing {} 0 true'.format(name, HIGHLIGHT_TIME))
 
 
 USERTP = {}
@@ -109,16 +55,17 @@ def user_tp_store_init(server):
 		with open(usertp) as f:
 			USERTP[usertp.stem] = json.load(f)
 
-	server.logger.info(f".tp 插件初始化 USERTP: ------>\n{USERTP}")
+	server.logger.debug(f".tp 插件初始化 USERTP: ------>\n{USERTP}")
 
 def player_save(player, data):
 	fullpathname = config_dir / (player + ".json")
 	with open(fullpathname, "w+") as f:
 			json.dump(data, f, ensure_ascii=False, indent=4)
 
-def click_text(player, label_name, x, y, z):
+def click_text(player, label_name, world, x, y, z):
 	r = RText(label_name, RColor.blue)
-	r.set_click_event(RAction.run_command, f"/execute at {player} run tp {x} {y} {z}")
+	r.set_hover_text(RText("点击传送", RColor.green))
+	r.set_click_event(RAction.run_command, f"/execute at {player} in {world} run tp {x} {y} {z}")
 	return r
 
 
@@ -129,23 +76,70 @@ def __get(src):
 def help(src):
 	server, info = __get(src)
 
-	server.tell(info.player, f"{'='*10} 使用方法 {'='*10}")
-	server.logger.info(f"{USERTP}")
+	msg=[f"{'='*10} 使用方法 {'='*10}",
+	".tp help                       查看使用方法",
+	".tp <收藏点>                    tp 到收藏点",
+	".tp list                       列出所有收藏点",
+	".tp add <收藏点名字>             添加当前位置为收藏点",
+	".tp remove <收藏点名字>          删除收藏点",
+	".tp rename <收藏点名字> <新名字>  修改收藏点名字",
+	]
+	server.reply(info, "\n".join(msg))
+
+	server.logger.debug(f"{USERTP}")
 
 def ls(src, ctx):
 	server, info = __get(src)
 
 	u = USERTP.get(info.player)
 
+	msg = [
+	f"{'='*10} 当前没有收藏点, 快使用下面命令收藏一个吧。 {'='*10}",
+	f"{cmdprefix} add <收藏点名>",
+	]
+
 	if u is None:
-		server.reply(info, f"{'='*10} 当前没有收藏点, 快使用下面命令收藏一个吧。 {'='*10}")
-		server.reply(info, f"{cmdprefix} add <收藏点名>")
+		server.reply(info, "\n".join(msg))
 	else:
-		server.reply(info, f"{'='*10} 收藏点 {'='*10}")
+		msg = [RText(f"{'='*10} 收藏点 {'='*10}\n", RColor.white)]
+
 		for label_name, data in u.items():
-			server.reply(info, click_text(info.player, label_name, data["x"], data["y"], data["z"]))
+			msg.append(click_text(info.player, label_name, data["world"], data["x"], data["y"], data["z"]))
+			msg.append("\n")
+
+		server.reply(info, RTextList(*msg))
 
 	server.logger.debug(f"list ctx -------------->\n{ctx}")
+
+def tp(src, ctx):
+	server, info = __get(src)
+
+	msg = [
+	f"{'='*10} 当前没有收藏点, 快使用下面命令收藏一个吧。 {'='*10}",
+	f"{cmdprefix} add <收藏点名>",
+	]
+
+	server.logger.info(f"tp() 执行了。")
+
+	u = USERTP.get(info.player)
+	if u is None:
+		server.reply(info, "\n".join(msg))
+	else:
+		label_name = ctx.get("label_name")
+		label = u.get(label_name)
+
+		server.logger.debug(f"label_name: {label}")
+
+		if label is None:
+			server.reply(info, f"没有 {label_name} 收藏点")
+		else:
+			world = label["world"]
+			x, y, z = label["x"], label["y"], label["z"]
+			server.execute(f"execute at {info.player} in {world} run tp {x} {y} {z}")
+			server.logger.debug(f"execute at {info.player} in {world} run tp {x} {y} {z}")
+
+		server.logger.debug(f"label_name: {label_name} 收藏点：  {label}")
+
 
 def add(src, ctx):
 	server, info = __get(src)
@@ -154,9 +148,9 @@ def add(src, ctx):
 	rcon_result = server.rcon_query(f"data get entity {info.player} Dimension")
 
 	if rcon_result is None:
-		prompt = "rcon 没有开启, 请分别server.properties, MCDR/config.yml 开启。"
+		prompt = RText("rcon 没有开启, 请分别server.properties, MCDR/config.yml 开启。", RColor.red)
 		server.logger.warning(prompt)
-		server.tell(info.player, f"{cmdprefix} 插件没有配置成功，请联系服主。")
+		server.tell(info.player, Rtext(f"{cmdprefix} 插件没有配置成功，请联系服主。", RColor.red))
 
 	world = re.match('{} has the following entity data: "(.*)"'.format(info.player), rcon_result).group(1)
 
@@ -164,21 +158,21 @@ def add(src, ctx):
 	rcon_result = server.rcon_query(f"data get entity {info.player} Pos")
 	position = re.search(r"\[.*\]", rcon_result).group()
 	x, y, z = position.strip("[]").split(",")
-	x, y, z = int(x.split(".")[0]), int(y.split(".")[0]), int(z.split(".")[0])
+	x, y, z = round(float(x.strip(" d")), 1), round(float(y.strip(" d")), 1), round(float(z.strip(" d")), 1)
 
 	u = USERTP.get(info.player)
 	if u is None:
 		u = {}
 		USERTP[info.player] = u
 	elif len(u) > PLAYER_MAX_POINT:
-		server.reply(info, f"收藏起点已到最大数： {PLAYER_MAX_POINT} 请删除后添加")
+		server.reply(info, RText(f"收藏起点已到最大数： {PLAYER_MAX_POINT} 请删除后添加", RColor.red))
 		return 
 
 	label_name = ctx["label_name"]
 
 	u[label_name] = {"world": world, "x": x, "y": y, "z":z}
 
-	server.tell(info.player, f"地点: {label_name} 收藏成功")
+	server.tell(info.player, RTextList("地点: ", RText(label_name, RColor.blue), " 收藏成功"))
 
 	player_save(info.player, u)
 
@@ -189,20 +183,20 @@ def remove(src, ctx):
 	server, info = __get(src)
 
 	if USERTP.get(info.player) is None:
-		server.tell(info.player, f"当前没有收藏点.")
+		server.tell(info.player, RText(f"当前没有收藏点.", RColor.red))
 	else:
 		u = USERTP.get(info.player)
 		label_name = ctx.get("label_name")
 
 		if label_name is None:
-			server.reply(info, f"没有 {label_name} 收藏点")
+			server.reply(info, RTextList(RText("没有 "), RText(label_name, RColor.blue), RText(" 收藏点")))
 			return
 
 		u.pop(label_name)
 
 		player_save(info.player, u)
 
-		server.reply(info, f"地点: {label_name} 删除成功")
+		server.reply(info, RTextList("地点: ", RText(label_name, RColor.blue), " 删除成功"))
 
 	server.logger.debug(f"remove ctx -------------->\n{ctx}")
 
@@ -210,7 +204,7 @@ def rename(src, ctx):
 	server, info = __get(src)
 
 	if USERTP.get(info.player) is None:
-		server.tell(info.player, f"当前没有收藏点.")
+		server.tell(info.player, RText(f"当前没有收藏点.", RColor.red))
 	else:
 		u = USERTP.get(info.player)
 
@@ -218,7 +212,7 @@ def rename(src, ctx):
 		label = u.get(label_name)
 
 		if label is None:
-			server.reply(info, f"没有 {label_name} 收藏点")
+			server.reply(info, RText(f"没有 {label_name} 收藏点", RColor.red))
 		else:
 			v = u.pop(label_name)
 			u[ctx["label_name2"]] = v
@@ -229,7 +223,9 @@ def rename(src, ctx):
 	server.logger.debug(f"rename ctx -------------->\n{ctx}")
 
 def build_command():
-	c = Literal(cmdprefix).runs(help)
+	#c = Literal(cmdprefix).then(Literal("tp").then(Text("label_name").runs(tp)))
+	c = Literal(cmdprefix).then(Text("label_name").runs(tp))
+	c.then(Literal("help").runs(help))
 	c.then(Literal("list").runs(ls))
 	c.then(Literal("add").then(QuotableText("label_name").runs(add)))
 	c.then(Literal("remove").then(Text("label_name").runs(remove)))
