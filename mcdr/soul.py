c1210fdad69d2a2797803543a02d19a2ca6d404a
calllivecn <calllivecn@outlook.com>
2021-02-09 23:09:54 +0800

    权限在这里测试成功。
    还需要添加的功能：
        1. 在玩家达到30级的时候，跟精灵学习后才能使用。

--- mcdr/soul.py
+++ mcdr/soul.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from mcdreforged.api.decorator import new_thread
 from mcdreforged.api.rtext import RText, RColor, RAction, RStyle, RTextList
 from mcdreforged.command.builder.command_node import Literal, QuotableText, Text, GreedyText, Integer
+from mcdreforged.permission.permission_level import PermissionLevel
 
 PLUGIN_METADATA = {
     # ID（即插件ID）是您插件的身份字符串。它应该由小写字母，数字和下划线组成，长度为1到64
@@ -47,54 +48,93 @@ def get_soul_info(player):
     if soul_player.exists():
         with open(soul_player) as f:
             return json.load(f)
+        os.remove(soul_player)
     else:
         return {}
+
+def set_soul_info(player, soul_info):
+    soul_player = SOUL_DIR / (player + ".json")
+    with open(soul_player, "w+") as f:
+        return json.dump(soul_info, f, ensure_ascii=False, indent=4)
+
+def __get(src):
+    return src.get_server(), src.get_info()
 
 
 def soul(server, info):
     # 查询游戏模式
-    result = server.rcon_query(f"data get enetity {info.player} Gamemode")
+    rcon_result = server.rcon_query(f"data get entity {info.player} playerGameType")
+
     if rcon_result is None:
         prompt = "rcon 没有开启, 请分别server.properties, MCDR/config.yml 开启。"
         server.logger.warning(prompt)
         server.reply(info, RText(f"{cmdprefix} 插件没有配置成功，请联系服主。", RColor.red))
-    
-    gamemode = re.match("还没写", result).group(1)
-    
-    if gamemode == "survival":
 
-    elif gamemode == "spectator":
+        return
+    
+    gamemode = re.match('{} has the following entity data: ([0-9]+)'.format(info.player), rcon_result).group(1)
+
+    # "Survival" == "0"
+    # "Creative" == "1"
+    # "Adventure" == "2"
+    # "Spectator" == "3"
+    if gamemode == "0":
+
+        # 查询世界
+        rcon_result = server.rcon_query(f"data get entity {info.player} Dimension")
+
+        world = re.match('{} has the following entity data: "(.*)"'.format(info.player), rcon_result).group(1)
+
+        # 查询坐标
+        rcon_result = server.rcon_query(f"data get entity {info.player} Pos")
+        position = re.search(f"{info.player} has the following entity data: \[(-?[0-9\.]+)d, (-?[0-9.]+)d, (-?[0-9.]+)d\]", rcon_result)
+        x, y, z = position.group(1), position.group(2), position.group(3)
+        x, y, z = round(float(x), 1), round(float(y), 1), round(float(z), 1)
+
+        set_soul_info(info.player, {"world": world, "x": x, "y": y, "z": z})
+
+        server.rcon_query(f"execute at {info.player} run gamemode spectator {info.player}")
+
+
+    elif gamemode == "3":
+        player_soul = get_soul_info(info.player)
+        if player_soul:
+            world = player_soul["world"]
+            x = player_soul["x"]
+            y = player_soul["y"]
+            z = player_soul["z"]
+
+            server.rcon_query(f"execute at {info.player} in {world} run teleport {info.player} {x} {y} {z}")
+            server.rcon_query(f"execute at {info.player} run gamemode survival {info.player}")
+
+        else:
+            server.reply(info, RText("出了点问题，请联系管理员。", RColor.red))
+            server.logger.warning(f"玩家 {info.player} 被卡在灵魂模式了。。。")
+
     
     else:
         server.reply(info, RText(f"哦~天哪，你不在五行中，我帮不了你...", RColor.green))
+        return
 
 
-    # 查询世界
-    rcon_result = server.rcon_query(f"data get entity {info.player} Dimension")
+def soul_runs(src, ctx):
+    server, info = __get(src)
 
-    world = re.match('{} has the following entity data: "(.*)"'.format(info.player), rcon_result).group(1)
+    perm = server.get_permission_level(info) 
+    #server.logger.info(f"info --> {info}\n permission --> {perm}")
 
-    # 查询坐标
-    rcon_result = server.rcon_query(f"data get entity {info.player} Pos")
-    position = re.search(r"\[.*\]", rcon_result).group()
-    x, y, z = position.strip("[]").split(",")
-    x, y, z = round(float(x.strip(" d")), 1), round(float(y.strip(" d")), 1), round(float(z.strip(" d")), 1)
-
+    if perm >= PermissionLevel.USER:
+        soul(server, info)
 
 def on_user_info(server, info):
-    permission = info.get_permission_level(info)
+    pass
 
-    if permission >= 1:
-        soul(server, info)
-    else:
-        server.reply(info, RText("没有权限", RColor.red))
-
-def on_player_joined(server, info):
-
+def on_player_joined(server, player, info):
+    pass
 
 def build_command():
-    return Literal(f"{cmdprefix}").runs(soul)
+    return Literal(f"{cmdprefix}").runs(soul_runs)
 
 def on_load(server, old_plugin):
-    server.register_help_message(cmdprefix, RText("招唤出你的灵魂", RColor.yellow))
+    server.register_help_message(cmdprefix, RText("招唤出你的灵魂", RColor.yellow), PermissionLevel.USER)
     server.register_command(build_command())
