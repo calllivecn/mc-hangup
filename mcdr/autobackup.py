e1fef39f255f3eae726efcd3edc75c6d67111c26
calllivecn <calllivecn@outlook.com>
2021-02-09 17:02:16 +0800

    save

--- mcdr/autobackup.py
+++ mcdr/autobackup.py
@@ -13,6 +13,7 @@ from mcdreforged import config
 from mcdreforged.api.decorator import new_thread
 from mcdreforged.api.rtext import RText, RColor, RAction, RStyle, RTextList
 from mcdreforged.command.builder.command_node import Literal, QuotableText, Text, GreedyText, Integer
+from mcdreforged.permission.permission_level import PermissionLevel
 
 PLUGIN_METADATA = {
     # ID（即插件ID）是您插件的身份字符串。它应该由小写字母，数字和下划线组成，长度为1到64
@@ -30,15 +31,22 @@ PLUGIN_METADATA = {
 
 cmdprefix = "." + "backup"
 
-cur_dir = Path(os.path.dirname(os.path.dirname(__file__))
-world_dir = cur_dir / "server" / "world"
+cur_dir = Path(os.path.dirname(os.path.dirname(__file__)))
 
+level_world = "world"
+world_dir = cur_dir / "server" / level_world
+backup_dir = cur_dir / "server"
+
+
+
+def build_command():
+    pass
 
 def on_unload(server):
     pass
 
 def on_load(server, old_plugin):
-    server.register_help_message(cmdprefix, PLUGIN_METADATA["name"])
+    server.register_help_message(cmdprefix, PLUGIN_METADATA["name"], PermissionLevel.ADMIN)
     server.register_command(build_command())
 
 def on_player_joined(server, player):
