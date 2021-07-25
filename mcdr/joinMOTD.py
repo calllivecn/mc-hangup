2bf8d9fc4a004685279b95dd28a709af888b5d29
calllivecn <calllivecn@outlook.com>
2021-07-25 17:20:24 +0800

    一些修改

---
+++ mcdr/joinMOTD.py
@@ -0,0 +1,60 @@
+# -*- coding: UTF-8 -*-
+
+from re import match
+
+from mcdreforged.api.decorator import new_thread
+from mcdreforged.api.command import Literal
+from mcdreforged.api.types import ServerInterface, Info, PlayerCommandSource
+from mcdreforged.api.rtext import RText, RTextTranslation, RTextList, RAction
+from mcdreforged.minecraft.rtext import RColor
+
+from funcs import (
+    CMDPREFIX,
+    CONFIG_DIR,
+    __get,
+)
+
+PLUGIN_METADATA = {
+    'id': 'joinmotd',
+    'version': '0.1',
+    'name': 'JoinMOTD',
+    'description': '',
+    'author': [
+        'calllivecn'
+    ],
+    'link': 'https://github.com/github/mc-hangup/mcdr/joinMOTD.py',
+    'dependencies': {
+        'mcdreforged': '>=1.3.0'
+    }
+}
+
+CMD = CMDPREFIX + PLUGIN_METADATA["id"]
+
+
+def welcome(server, player):
+    msg = [
+        RText("="*10 + "嗨～！" + "="*10 + "\n\n"),
+        RText(f"欢迎！ {player} ！\n\n", RColor.yellow),
+    ]
+
+    r = RText(">>> 点击这里，查看可用命令 <<<", RColor.green)
+    r.set_hover_text("!!help")
+    r.set_click_event(RAction.run_command, f"!!help")
+
+    msg.append(r)
+
+    server.tell(player, RTextList(*msg))
+
+def welcome_cmd(src, ctx):
+    server, info = __get(src)
+    welcome(server, info.player)
+
+def on_player_joined(server, player, info):
+    welcome(server, player)
+
+def on_load(server: ServerInterface, prev):
+
+    server.register_help_message(CMD, "欢迎信息")
+    server.register_command(
+        Literal(CMD).runs(welcome_cmd)
+    )
