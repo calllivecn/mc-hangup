968956a87a45e3e0dce6049232dfe7476dbc894d
calllivecn <calllivecn@outlook.com>
2021-09-01 19:01:57 +0800

    测试中

--- mc-fishing-opencv/SIFT.py
+++ mc-fishing-opencv/SIFT.py
@@ -13,10 +13,10 @@ from funcs import (
 
 images_path = Path("images")
 
-img_temp = cv2.imread(str(images_path / 'temp-big.png'))#读取原图片
-img_target = cv2.imread(str(images_path / 'target-orb.png'))#读取旋转图片
-img_match1 = cv2.imread(str(images_path / 'match1.png'))#读取缩放图片
-img_match2 = cv2.imread(str(images_path / 'match2.png'))
+img_temp = cv2.imread(str(images_path / 'mc-fishing_1920x1080.png'))#读取原图片
+img_target = cv2.imread(str(images_path / 'mc-fishing.png'))#读取旋转图片
+img_match1 = cv2.imread(str(images_path / 'target_img.png'))#读取缩放图片
+img_match2 = cv2.imread(str(images_path / 'target.png'))
 
 imgs = [img_temp, img_target, img_match1, img_match2 ]
 
@@ -33,13 +33,33 @@ for img in imgs:
     binary_imgs.append(dst_temp)
     binarys_retvals.append(retval_temp)
 
+# 边缘检测，框出物体的轮廓
+con = []
+for img in gray_imgs:
+    #canny = cv2.canny(img）
+    #图像去噪：使用高斯滤波cv2.GaussianBlur()
+    gau = cv2.GaussianBlur(img, (3, 3), 1)
+    ret, binary = cv2.threshold(gau, 80, 255, cv2.THRESH_BINARY_INV)
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, element)
+    con.append(binary)
+
+imgshow(img_temp, con)
+
 
 #ORB 特征提取
 a=time.time()
 sift = cv2.SIFT_create()
 sifts = []
-for img in binary_imgs:
+#for img in binary_imgs:
+
+# 使用灰度图提取特征
+#for img in gray_imgs:
+
+# 使用原RGB图
+for img in imgs:
     kp1, des1 = sift.detectAndCompute(img, None)
+    print(f"pk 计数: {len(kp1)}, des 计数：{len(des1)}")
     sifts.append((kp1, des1))
     # print(f"pk: {kp1} des: {des1}")
 
@@ -79,6 +99,9 @@ for i, sift_ in enumerate(sifts):
         if(m.distance < n.distance*0.75):
             good.append([m])
 
+    print(f"特征匹配：{len(good)}")
+
+    # 接连匹配的特征点，并展示。
     # img = cv2.drawMatches(img1, pk1, img4, kp4, matches[:50], img4, flags=2)
     img = cv2.drawMatchesKnn(img_temp, temp_pk, imgs[i], sift_[0], good, None, flags=2)
     sift_matchs.append(img)
