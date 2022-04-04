3c9c653a5a06cc7bf52eb717e4133d1ac861cd72
calllivecn <calllivecn@outlook.com>
2022-04-04 21:22:19 +0800

    使用算法，找身上只有一层{}的物品。

--- mcdr/autohealth/autohealth.py
+++ mcdr/autohealth/autohealth.py
@@ -6,6 +6,7 @@
 
 import re
 import time
+import copy
 
 from funcs import (
     CMDPREFIX,
@@ -58,17 +59,17 @@ if Interval < 1:
 elif Interval > 15:
     Interval = 15
 
-HealthPercentage = conf.getfloat("autohealth", "Interval")
+# HealthPercentage = conf.getfloat("autohealth", "HealthPercentage")
 
 
-FOOD = {}
+FOOD = []
 for node in conf.sections():
 
     if node == "autohealth":
         continue
 
     for k, v in conf.items(node):
-        FOOD[":".join([node, k])] =  int(v)
+        FOOD.append((":".join([node, k]), int(v)))
 
 """
 FOOD = {
@@ -83,51 +84,115 @@ FOOD = {
 # 结束flag
 EXIT=False
 
-def check_food(server, player):
-    text = server.rcon_query(f"data get entity {player} Inventory")
+## 只找只有一层{}的物品
+def item_body(result):
+    items=[]
+    stack = []
+    start = 0 
+    end = 0
+    stack1_flag=True
 
-    for food, value in FOOD.items():
-        count = re.findall(f"""Slot: ([0-9]+)b, id: "{food}", Count: ([0-9]+)b""", text)
+    for i, c in enumerate(result):
+        if c == "{":
+            stack.append("}")
+            start = i
+
+        elif c == "}":
+            if stack1_flag and len(stack) == 1:
+                end = i
+                stack.pop()
+                items.append(result[start:end+1])
+
+            elif not stack1_flag and len(stack) == 1:
+                stack1_flag = True
+                start, end = i, i 
+                stack.pop()
+
+            elif len(stack) > 1:
+                start, end = i, i 
+                stack1_flag=False
+                stack.pop()
+    
+    return ",".join(items)
+
+
+def check_food(server, player, food_local):
+    result = server.rcon_query(f"data get entity {player} Inventory")
+
+    text = item_body(result)
+
+    server.logger.debug(f"FOOD: {food_local}")
+
+    for index, (food, value) in enumerate(food_local):
+        re_findall=f"""{{Slot: ([0-9]+)b, id: "{food}", Count: ([0-9]+)b}}"""
+        server.logger.debug(re_findall)
+        count = re.findall(re_findall, text)
+
         total = sum([int(x[1]) for x in count])
+        server.logger.debug(f"查找玩家 {player} 回血食物 {food}：{total}")
         if total >= value:
-            server.logger.debug(f"玩家 {player} 回血食物 {food}：{total}")
-            return food
+            # 当前找到后，把它放到最前面，
+            food_local[0], food_local[index] = food_local[index], food_local[0]
+            return True
     
     return None
 
 
+# 这样不行，clear 是最多减 v 个。
+def clear_food(server, player, food_local):
+
+    for index, (food, v) in enumerate(food_local):
+        result = server.rcon_query(f"clear {player} {food} {v}")
+        if re.match(f"No items were found on player {player}", result):
+            continue
+        else:
+            # 当前找到后，把它放到最前面，
+            food_local[0], food_local[index] = food_local[index], food_local[0]
+            return True
+
+    return False
+
+
 @new_thread("auto health")
 def add_health(server, player, number):
+    # copy 一份 FOOD
+    food_local = copy.deepcopy(FOOD)
+
     server.logger.info(f"{player} 自动回血技能启动。")
     server.tell(player, RText(f"自动回血技能启动", RColor.yellow))
 
     # 拿到玩家当前的最大血量
     result = server.rcon_query(f"attribute {player} minecraft:generic.max_health base get")
-    re_match = re.match(f"Base value of attribute Max Health for entity calllivecn is (.*)", result)
+
+    re_match = re.match(f"Base value of attribute Max Health for entity {player} is (.*)", result)
     max_health = float(re_match.group(1))
 
     global EXIT
     while EXIT:
         #calllivecn has the following entity data: 20.0f
         text = server.rcon_query(f"data get entity {player} Health")
+
+        if text is None:
+            server.logger.debug(f"玩家{player}下线")
+            return 
+
         result = re.match(f"""{player} has the following entity data: (.*)f""", text)
         health = float(result.group(1))
         cmp = health / max_health
         if cmp <= number:
-            food = check_food(server, player)
-            if food:
-                result = server.rcon_query(f"clear {player} {food} {FOOD[food]}")
-                if re.match(f"No items were found on player {player}", result):
-                    server.tell(player, RText(f"背包当前食物不够了!!!", RColor.red))
-                    EXIT=False
-                else:
-                    # server.rcon_query(f"effect give {player} minecraft:instant_health 1 {FOOD[food]} true")
-                
-                    # 状态效果等级是从0开始的
-                    server.rcon_query(f"effect give {player} minecraft:instant_health 1 0 true")
-                    # server.rcon_query(f"effect give {player} minecraft:regeneration 1 3 true") 不行，太慢
-                    # /effect give calllivecn minecraft:regeneration 1 3 true
-                    time.sleep(0.2)
+
+            index = check_food(server, player, food_local)
+            food, v = food_local[0]
+            if index:
+                server.rcon_query(f"clear {player} {food} {v}")
+                # result = server.rcon_query(f"clear {player} {food} {v}")
+                # if re.match(f"No items were found on player {player}", result):
+
+                # /effect give calllivecn minecraft:regeneration 1 3 true
+                # 状态效果等级是从0开始的
+                server.rcon_query(f"effect give {player} minecraft:instant_health 1 0 true")
+                # server.rcon_query(f"effect give {player} minecraft:regeneration 1 3 true") 不行，太慢
+                time.sleep(0.1)
             else:
                 server.tell(player, RText(f"背包当前食物不够了!!!", RColor.red))
                 EXIT=False
