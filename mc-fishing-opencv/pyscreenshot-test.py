a665e8a36d1d7b4e713dba4403df248a645f1219
calllivecn <c-all@qq.com>
2023-06-26 03:34:13 +0800

    小调整

--- mc-fishing-opencv/pyscreenshot-test.py
+++ mc-fishing-opencv/pyscreenshot-test.py
@@ -12,7 +12,8 @@ import pyscreenshot
 
 t1 = time.time()
 for i in range(10):
-    img = pyscreenshot.grab(bbox=(0, 0, 400, 800), backend="mss")
+    #img = pyscreenshot.grab(bbox=(0, 0, 400, 800), backend="mss")
+    img = pyscreenshot.grab(bbox=(0, 0, 400, 800))
 t2 = time.time()
 print(img)
 print("平均一次耗时：", round((t2-t1)/10, 3), "/s")
